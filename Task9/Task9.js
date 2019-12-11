32d79ebb4a0cac709b9334598be16d35aedac6b3
faisalqureshi <faisalqureshi690@gmail.com>
2019-12-11 10:50:22 +0500

    This Ninth Task Files

---
+++ Task9/Task9.js
@@ -0,0 +1,10 @@
+var a=20;
+var b=10;
+var c=a-b;
+document.write("<h3>"+"Subtraction of " + a + " and " + b + " is " +c +"</h3>" )
+var c=a*b;
+document.write("<h3>"+"Multiplication of " + a + " and " + b + " is " +c+"</h3>")
+var c=a/b;
+document.write("<h3>"+"Division of " + a + " and " + b + " is " +c+"</h3>")
+var c=a%b;
+document.write("<h3>"+"Modules of " + a + " and " + b + " is " +c+"</h3>")
