3fc6548996e7782e309b543c7e24023f6bbae638
faisalqureshi <faisalqureshi690@gmail.com>
2019-12-11 10:50:49 +0500

    This is Tenth Task Files

---
+++ Task10/Task10.js
@@ -0,0 +1,12 @@
+var a;
+document.write("<h3>"+"Value after variable declaration is:" + a+"</h3>")
+a=5;
+document.write("<h3>"+"Initial value:"+a + "</h3>")
+a++;
+document.write("<h3>"+"Value after increament is:"+a+"</h3>")
+a=a+7;
+document.write("<h3>"+"Value after addition is:"+a+"</h3>")
+a--;
+document.write("<h3>"+"Value after decreament is:"+a+"</h3>")
+a=a%3;
+document.write("<h3>"+"The remainder is is:"+a+"</h3>")
